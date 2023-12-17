7c0bb4343673d8f05407eecb239ac5b95c03d973
PaPi <89279156+Mesrine67@users.noreply.github.com>
2023-12-17 16:38:25 +0100

    Create version.lua

---
+++ version.lua
@@ -0,0 +1,6 @@
+-- version.lua
+return {
+    version = "1.2.3",
+    update_message = "Nouvelle mise à jour disponible : corrections de bugs et améliorations.",
+    update_url = "https://github.com/monrepo/mareource/releases/tag/1.2.3"
+}
